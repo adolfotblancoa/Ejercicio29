fd564de75f5d0132a3322d35f650f1b81c18944e
adolfotblancoa <115952570+adolfotblancoa@users.noreply.github.com>
2022-10-28 11:52:11 +0200

    Add files via upload

---
+++ Ejercicio29/src/dominio/Localidad.java
@@ -0,0 +1,29 @@
+package dominio;
+public class Localidad
+{
+String nombre;
+int numeroDeHabitantes;
+public String getNombre()
+{
+return nombre;
+}
+public void setNombre(String nombre)
+{
+this.nombre = nombre;
+}
+public int getNumeroDeHabitantes()
+{
+return numeroDeHabitantes;
+}
+public void setNumeroDeHabitantes(int
+numeroDeHabitantes)
+{
+this.numeroDeHabitantes =
+numeroDeHabitantes;
+}
+public String toString()
+{
+return nombre + " " + numeroDeHabitantes
++ " habitantes";
+}
+}
